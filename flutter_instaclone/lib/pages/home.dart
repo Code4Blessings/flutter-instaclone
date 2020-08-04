dfc3f9cc863cf743760276cd2caf719f2ce2ce97
Code4Blessings <robincw35@gmail.com>
2020-08-04 13:52:29 -0400

    Added styling to the home page

--- flutter_instaclone/lib/pages/home.dart
+++ flutter_instaclone/lib/pages/home.dart
@@ -10,11 +10,51 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Trendy Girl'),
+        title: Text(
+          'Trendy Girl',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black
+          ),
+        ),
         backgroundColor: Colors.green,
         centerTitle: true,
       ),
-      body: Text('You got this girl!!!'),
+      body: Center(
+        child: Text(
+          'You Got This Girl',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+          '+',
+          style: TextStyle(
+             fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.black
+          ),
+        ),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
+
+class test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
